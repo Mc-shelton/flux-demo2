d75a6b6662ec92065f0f899a8efacc3a33b56735
Mc-shelton <sheltonnito@gmail.com>
2023-11-26 12:36:36 +0300

    initial commit

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import express from 'express'
+import os from 'os'
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    const hostname = os.hostname()
+    res.send(`Welcome to Devops Community : ${hostname}`)
+})
+
+app.listen(3230,()=>{
+    console.log('app listening on port 3230!')
+})
